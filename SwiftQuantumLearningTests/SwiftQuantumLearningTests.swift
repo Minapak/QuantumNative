e71a0bdd9ec796779b2f257c4e59457e3e63e234
Minapak <dmsals2008@gmail.com>
2025-12-31 07:28:56 +0900

    Initial commit: SwiftQuantum Learning App

---
+++ SwiftQuantumLearningTests/SwiftQuantumLearningTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftQuantumLearningTests.swift
+//  SwiftQuantumLearningTests
+//
+//  Created by EUNMIN_P on 12/31/25.
+//
+
+import Testing
+
+struct SwiftQuantumLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
